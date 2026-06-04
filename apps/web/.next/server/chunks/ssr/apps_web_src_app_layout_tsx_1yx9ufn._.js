15a3f5ec5ffd88bd8b771f1dd499e0573414f1a7
poco0222 <popo.dd.0222@gmail.com>
2026-06-04 15:12:49 +0800

    清理Task1误纳入的构建产物

--- apps/web/.next/server/chunks/ssr/apps_web_src_app_layout_tsx_1yx9ufn._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[77930,a=>{"use strict";var b=a.i(96197);a.s(["default",0,function({children:a}){return(0,b.jsx)("html",{lang:"en",children:(0,b.jsx)("body",{children:a})})},"metadata",0,{title:"POCO Scrum Platform",description:"Phase 1 bootstrap shell for the Scrum-first workspace."}])},34005,a=>{a.n(a.i(77930))}];
-
-//# sourceMappingURL=apps_web_src_app_layout_tsx_1yx9ufn._.js.map
